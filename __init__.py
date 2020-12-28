2f3dd7e32d307881ef97b2cbf6392cb0af1270fe
Andi <andi@ahlbach.net>
2020-12-28 21:53:50 +0100

    init commit

---
+++ __init__.py
@@ -0,0 +1,34 @@
+from PIL import Image
+from shutil import copyfile
+import imagehash
+
+import glob, os
+
+IMG_DIR = 'img'
+OUT_DIR = 'out'
+
+os.chdir(IMG_DIR)
+if not os.path.isdir(OUT_DIR):
+    os.mkdir(OUT_DIR)
+all_files_dict = {}
+
+print('scan all files...')
+
+for file in glob.glob("*"):
+    if not os.path.isfile(file):
+        continue
+    hash = imagehash.average_hash(Image.open(file))
+    size = os.path.getsize(file)
+    print(file, hash, size)
+    if hash in all_files_dict:
+        if all_files_dict[hash]['size'] > size:
+            # We found a smaller file than before.
+            continue
+
+    # We found a new or bigger file. Update file infos.
+    all_files_dict[hash] = {'size': size, 'file': file}
+
+for hash in all_files_dict:
+    file = all_files_dict[hash]['file']
+    print('copy to out: ', file)
+    copyfile(file, OUT_DIR + '/' + file)
