babab38079ac12a23a0fc1222aaf83b2e2adecaf
Andreas('Schmidt') <6840700+myaicloud@users.noreply.github.com>
2020-12-28 22:05:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 from PIL import Image
 from shutil import copyfile
 import imagehash
-
 import glob, os
 
 IMG_DIR = 'img'
 OUT_DIR = 'out'
 
+# Initialize directories.
 os.chdir(IMG_DIR)
 if not os.path.isdir(OUT_DIR):
     os.mkdir(OUT_DIR)
+
 all_files_dict = {}
 
 for file in glob.glob("*"):
@@ -20,12 +21,13 @@ for file in glob.glob("*"):
     print(file, hash, size)
     if hash in all_files_dict:
         if all_files_dict[hash]['size'] > size:
-            # We found a smaller file than before.
+            # Found a smaller file than before.
             continue
 
-    # We found a new or bigger file. Update file infos.
+    # Found a new or bigger file. Update file informations.
     all_files_dict[hash] = {'size': size, 'file': file}
 
+# Copy all files from resultset into the OUT_DIR directory.
 for hash in all_files_dict:
     file = all_files_dict[hash]['file']
     print('final file: ', file)
